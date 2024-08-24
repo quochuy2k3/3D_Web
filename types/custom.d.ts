bb3fbdec37e0a85de88ece2fee702fb3dd63a446
quochuy2k3 <voquochuy3006@gmail.com>
2024-08-24 22:01:28 +0700

    1st commit

---
+++ types/custom.d.ts
@@ -0,0 +1,18 @@
+declare module '*.glb' {
+    const value: string;
+    export default value;
+  }
+  
+declare module '*.fbx' {
+    const value: string;
+    export default value;
+  }  
+  declare module '*.obj' {
+    const value: string;
+    export default value;
+  } 
+  declare module '*.glsl' {
+    const value: string;
+    export default value;
+  }
+  
